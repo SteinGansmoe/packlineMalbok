bb75e7addf5e1a1a9a5e487575ad381bf9d1a358
SteinGansmoe <steina89@hotmail.com>
2026-01-18 20:00:48 +0100

    Basic login function complete.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react';
+import { supabase } from "./lib/supabaseClient";
+import type { Session } from "@supabase/supabase-js";
+import Login from "./pages/Login"; 
 
 function App() {
   const [session, setSession] = useState<Session | null>(null);
   const [loading, setLoading] = useState(true);
 
+  useEffect(() => {
+    const fetchSession = async () => {
+      try {
+    const { data, error } = await supabase.auth.getSession();
+    if (error) {
+      throw new Error("Kunne ikke hente session, logg inn på nytt.");
+    } 
+    setSession(data.session);
+    
+      } catch (error) {
+        console.error("Error fetching session:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchSession();
+
+    const { data: { subscription } } = supabase.auth.onAuthStateChange(
+      (_event, session) => {
+        setSession(session);
+      }
+    );
+    return () => subscription.unsubscribe();
+  },[])
+
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
+  }
+
+  if (loading) return <div>Laster...</div>;
+
   return (
     <div>
-      {/* Content to go here */}
+      {!session ? (
+         <Login/>
+      ) : (
+        <>
+        <p>Velkommen, {session.user.email}</p>
+        <button onClick={handleLogout}>Logg ut</button>
+      </>
+      )}
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import App from "./App.tsx";
 
-import Login from './pages/login';
-import "./lib/supabaseClient"; // REMOVE LATER WHEN CLIENT IS READY
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Login />
+    <App />
   </StrictMode>
 )
